1f7f6a5d4cca32013a0f0d687877346cd5b0e5da
prassr <92257326+prassr@users.noreply.github.com>
2026-03-27 18:54:18 +0530

    08-Dependency Injection: Router Integration to ProductController

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -7,21 +7,21 @@ namespace App\Controllers;;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
-# Using factory
-use GuzzleHttp\Psr7\HttpFactory;
-use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseFactoryInterface;
+
 
 
 class ProductController 
 {
+    public function __construct(private ResponseFactoryInterface $factory)
+    {}
+    
     public function index(): ResponseInterface
     {
-
-        $factory = new HttpFactory;
         // for sending body
-        $stream = $factory->createStream("List of Products");
+        $stream = $this->factory->createStream("List of Products");
 
-        $response = $factory->createResponse();
+        $response = $this->factory->createResponse();
 
         $response = $response->withBody($stream);
 
@@ -30,16 +30,13 @@ class ProductController
 
     public function  show(ServerRequestInterface $request, array $args): ResponseInterface
     {
-
-        $factory = new Psr17Factory;
-
         /* $id = $request->getQueryParams()["id"]; */
         $id = $args["id"];
 
         // for sending body
-        $stream = $factory->createStream("Single product id $id");
+        $stream = $this->factory->createStream("Single product id $id");
 
-        $response = $factory->createResponse();
+        $response = $this->factory->createResponse();
 
         $response = $response->withBody($stream);
 
