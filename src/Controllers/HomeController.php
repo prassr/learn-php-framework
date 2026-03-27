de5783db9f39376d8ef6854bdf07cb160b683751
prassr <92257326+prassr@users.noreply.github.com>
2026-03-27 17:58:41 +0530

    07: HTTP Factories: Using only Factory methods in HomeController

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -5,18 +5,13 @@ declare(strict_types=1);
 
 namespace App\Controllers;;
 
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\Utils;
 # using below interface for psr helps to avoid errors when changing between packages
 # which implement this interface.
 # Since, we have used this in response type,
 # it should not throw any errors when changing the package
 # from GuzzleHttp to nyholm/psr7
 use Psr\Http\Message\ResponseInterface;
-use Nyholm\Psr7\Stream;
-
 # Using factory
-
 use GuzzleHttp\Psr7\HttpFactory;
 use Nyholm\Psr7\Factory\Psr17Factory;
 
